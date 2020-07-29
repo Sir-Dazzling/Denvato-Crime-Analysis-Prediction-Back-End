bd8d0416c12040945bcb6bf0f587b6284bb9bb8b
Otti Daniel <33727291+Sir-Dazzling@users.noreply.github.com>
2020-07-29 07:58:20 +0100

    added jackson and json jwt token dependencies

---
+++ src/main/java/com/example/denvatocrimeanalyticsplatform/DenvatocrimeanalyticsplatformApplication.java
@@ -0,0 +1,14 @@
+package com.example.denvatocrimeanalyticsplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenvatocrimeanalyticsplatformApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(DenvatocrimeanalyticsplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/denvatocrimeanalyticsplatform/DenvatocrimeanalyticsplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.denvatocrimeanalyticsplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenvatocrimeanalyticsplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
